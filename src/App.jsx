8d8d7bd79addbbd3a3b0e1d8825d0a5c9164e311
ayyanovia1121 <noviayya1121@gmail.com>
2024-09-16 12:42:35 +0700

    React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+
 import './index.css'
+import { Outlet } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='text-red-50'>Halloww</h1>
+     <Outlet />
     </>
   )
 }
